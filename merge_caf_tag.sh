7164a0ee38e37906a88d43d86fc37d509329314e
Karan Parashar <whyredfire@gmail.com>
2021-06-01 00:48:01 +0530

    Update merge_caf_tag.sh

--- merge_caf_tag.sh
+++ merge_caf_tag.sh
@@ -8,7 +8,7 @@ echo -e ""
 
 # qcald-3.0
 echo -e "Merging qcald-3.0"
-git remote add qcacld-3.0 https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0
+git remote add qcald-3.0 https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0
 git fetch qcacld-3.0 $TAG
 git merge -X subtree=drivers/staging/qcacld-3.0 FETCH_HEAD
 
@@ -27,7 +27,3 @@ echo -e "Merging qca-wifi-host-cmn"
 git remote add qca-wifi-host-cmn https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn
 git fetch qca-wifi-host-cmn $TAG
 git merge -X subtree=drivers/staging/qca-wifi-host-cmn FETCH_HEAD
-
-echo -e ""
-
-echo -e "CAF Tag $TAG merged successfully!"
