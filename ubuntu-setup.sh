f56c1286671c96443df43cec83e7cfd24bb963f2
Karan Parashar <whyredfire@gmail.com>
2021-06-22 09:43:13 +0530

    script: update gitconfig

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -91,7 +91,7 @@ echo "set-option -g history-limit 6000" >> .tmux.conf
 echo -e "\nSetting up Git..."
 
 git config --global user.email "whyredfire@gmail.com"
-git config --global user.name "whyredfire"
+git config --global user.name "Karan Parashar"
 
 git config --global alias.cp 'cherry-pick'
 git config --global alias.c 'commit'
