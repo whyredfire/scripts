9fd4a67d1fb2916f0f8b21b49e27fe381f2e79c7
Karan Parashar <whyredfire@gmail.com>
2022-03-03 16:45:36 +0530

    ubuntu-setup: Setup git config for gerrit

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -198,6 +198,7 @@ echo -e "\nSetting up Git..."
 
 git config --global user.email "whyredfire@gmail.com"
 git config --global user.name "Karan Parashar"
+git config --global review.gerrit.pixelexperience.org.username "whyredfire"
 
 git config --global alias.cp 'cherry-pick'
 git config --global alias.c 'commit'
