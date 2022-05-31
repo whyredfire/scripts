ea9cb38ae4b1d4b347457c5fc4da45575fa4b31e
Karan Parashar <whyredfire@gmail.com>
2022-05-31 08:29:38 +0530

    scripts: log merged changes

--- merge_caf_tag.sh
+++ merge_caf_tag.sh
@@ -10,7 +10,7 @@ echo -e ""
 echo -e "Merging qcacld-3.0"
 git remote add qcacld-3.0 https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0
 git fetch qcacld-3.0 $TAG
-git merge -X subtree=drivers/staging/qcacld-3.0 FETCH_HEAD
+git merge -X subtree=drivers/staging/qcacld-3.0 FETCH_HEAD --log
 
 echo -e ""
 
@@ -18,7 +18,7 @@ echo -e ""
 echo -e "Merging fw-api"
 git remote add fw-api https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/fw-api
 git fetch fw-api $TAG
-git merge -X subtree=drivers/staging/fw-api FETCH_HEAD
+git merge -X subtree=drivers/staging/fw-api FETCH_HEAD --log
 
 echo -e ""
 
@@ -26,10 +26,10 @@ echo -e ""
 echo -e "Merging qca-wifi-host-cmn"
 git remote add qca-wifi-host-cmn https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn
 git fetch qca-wifi-host-cmn $TAG
-git merge -X subtree=drivers/staging/qca-wifi-host-cmn FETCH_HEAD
+git merge -X subtree=drivers/staging/qca-wifi-host-cmn FETCH_HEAD --log
 
 # techpack
 echo -e "Merging techpack"
 git remote add teckpack https://git.codelinaro.org/clo/la/platform/vendor/opensource/audio-kernel
 git fetch techpack $TAG
-git merge -X subtree=techpack/audio FETCH_HEAD
+git merge -X subtree=techpack/audio FETCH_HEAD --log
