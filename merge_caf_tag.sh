884c6f05a58ea4c79174d3ceeb0407558985fa16
Karan Parashar <whyredfire@gmail.com>
2022-07-29 22:13:34 +0530

    scripts/caf-merge: Refractor
    Change-Id: I6d470adda555d56b9dc9bfee042801a92e85d638

--- merge_caf_tag.sh
+++ merge_caf_tag.sh
@@ -1,35 +1,79 @@
 # Script for merging caf tag
 # Copyright (C) 2021-22 Karan Parashar.
 
-echo -e "Paste the desirable caf tag"
+echo "Paste the desirable caf tag"
 read TAG
 
-echo -e ""
-
 # qcacld-3.0
-echo -e "Merging qcacld-3.0"
-git remote add qcacld-3.0 https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0
-git fetch qcacld-3.0 $TAG
-git merge -X subtree=drivers/staging/qcacld-3.0 FETCH_HEAD --log
+function merge_qcacld() {
+    echo "Merging qcacld-3.0"
+    if ! git remote add qcacld-3.0 https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0; then
+        git remote rm qcacld-3.0
+        git remote add qcacld-3.0 https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0
+    fi
 
-echo -e ""
+    git fetch qcacld-3.0 $TAG
+
+    if ! git merge -X subtree=drivers/staging/qcacld-3.0 FETCH_HEAD --log; then
+        echo "Merge failed!"
+    else
+        echo "Merged qcacld-3.0 tag sucessfully!"
+    fi
+}
 
 # fw-api
-echo -e "Merging fw-api"
-git remote add fw-api https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/fw-api
-git fetch fw-api $TAG
-git merge -X subtree=drivers/staging/fw-api FETCH_HEAD --log
+function merge_fw_api() {
+    echo "Merging fw-api"
+    if ! git remote add fw-api https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/fw-api; then
+        git remote rm fw-api
+        git remote add fw-api https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/fw-api
+    fi
 
-echo -e ""
+    git fetch fw-api $TAG
+
+    if ! git merge -X subtree=drivers/staging/fw-api FETCH_HEAD --log; then
+        echo "Merge failed!"
+    else
+        echo "Merged fw-api tag sucessfully!"
+    fi
+}
 
 # qca-wifi-host-cmn
-echo -e "Merging qca-wifi-host-cmn"
-git remote add qca-wifi-host-cmn https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn
-git fetch qca-wifi-host-cmn $TAG
-git merge -X subtree=drivers/staging/qca-wifi-host-cmn FETCH_HEAD --log
+function merge_qca_wifi_host_cmn() {
+    echo "Merging qca-wifi-host-cmn"
+    if ! git remote add qca-wifi-host-cmn https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn; then
+        git remote rm qca-wifi-host-cmn
+        git remote add qca-wifi-host-cmn https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn
+    fi
+
+    git fetch qca-wifi-host-cmn $TAG
+
+    if ! git merge -X subtree=drivers/staging/qca-wifi-host-cmn FETCH_HEAD --log; then
+        echo "Merge failed!"
+    else
+        echo "Merged qca-wifi-host-cmn tag sucessfully!"
+    fi
+}
 
 # techpack
-echo -e "Merging techpack"
-git remote add techpack https://git.codelinaro.org/clo/la/platform/vendor/opensource/audio-kernel
-git fetch techpack $TAG
-git merge -X subtree=techpack/audio FETCH_HEAD --log
+function merge_techpack() {
+    echo "Merging techpack"
+    if ! git remote add techpack https://git.codelinaro.org/clo/la/platform/vendor/opensource/audio-kernel; then
+        git remote rm techpack
+        git remote add techpack https://git.codelinaro.org/clo/la/platform/vendor/opensource/audio-kernel
+    fi
+
+    git fetch techpack $TAG
+
+    if ! git merge -X subtree=techpack/audio FETCH_HEAD --log; then
+        echo "Merge failed!"
+    else
+        echo "Merged techpack tag sucessfully!"
+    fi
+}
+
+# initialize script
+merge_qcacld
+merge_fw_api
+merge_qca_wifi_host_cmn
+merge_techpack
