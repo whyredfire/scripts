00d52a13e0037845ab0205c7bf2b971b29223af1
Adithya <gh0strider.2k18.reborn@gmail.com>
2022-09-15 20:23:48 +0530

    scripts: Add alias for git merge
    Change-Id: I9317484d257b1eb1dca10ad1d5d91d73d287c979

--- fedora-workstation.sh
+++ fedora-workstation.sh
@@ -102,6 +102,7 @@ git config --global user.name "Karan Parashar"
 git config --global alias.cp 'cherry-pick'
 git config --global alias.c 'commit'
 git config --global alias.f 'fetch'
+git config --global alias.m 'merge'
 git config --global alias.rb 'rebase'
 git config --global alias.rs 'reset'
 git config --global alias.ck 'checkout'
--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -213,6 +213,7 @@ git config --global review.gerrit.pixelexperience.org.username "whyredfire"
 git config --global alias.cp 'cherry-pick'
 git config --global alias.c 'commit'
 git config --global alias.f 'fetch'
+git config --global alias.m 'merge'
 git config --global alias.rb 'rebase'
 git config --global alias.rs 'reset'
 git config --global alias.ck 'checkout'
