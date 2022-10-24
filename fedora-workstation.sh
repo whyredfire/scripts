59ed811d5013e03f167eeebf33040eaa04976f9e
Karan Parashar <whyredfire@gmail.com>
2022-10-24 17:04:56 +0530

    fedora-workstation: Disable CPU turbo boost by default
    Change-Id: If6bbf80469d7cb43c52582326c53ccd820b876f3

--- fedora-workstation.sh
+++ fedora-workstation.sh
@@ -137,3 +137,6 @@ echo "Done."
 # gnome shell presets
 echo -e "\nSetting gnome shell presets..."
 gsettings set org.gnome.desktop.interface show-battery-percentage true
+
+# Disable CPU Boosting (for AMD CPUs)
+echo "0" | sudo tee /sys/devices/system/cpu/cpufreq/boost
