ecee7dfffae807e5558c33e4425fdf6e8c986458
Karan Parashar <whyredfire@gmail.com>
2022-10-31 05:49:16 +0530

    fedora-workstation: Recover window buttons in gnome
    Change-Id: I04a65b3d512dfcabc38085f43c673f7ceaad0936

--- fedora-workstation.sh
+++ fedora-workstation.sh
@@ -141,6 +141,8 @@ gsettings set org.gnome.desktop.interface show-battery-percentage true
 gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+
 
 # Disable CPU Boosting (for AMD CPUs)
 echo "0" | sudo tee /sys/devices/system/cpu/cpufreq/boost
