b56fbf1832da115f1365b6ccad1cab5a8dd9fd6c
Karan Parashar <karan@pixelos.net>
2023-02-22 10:14:57 +0530

    fedora-setup: Group dnf packages
    Change-Id: If0efdb4eecf665d719e6d04581748409fc63b848

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -19,47 +19,13 @@ cd $HOME
 echo -e "Installing and updating dnf packages...\n"
 sudo dnf update -qq
 sudo dnf install -y -qq \
-    android-tools \
-    autoconf213 \
-    bison \
-    bzip2 \
-    ccache \
-    curl \
-    flex \
-    gawk \
-    gcc-c++ \
-    git \
-    glibc-devel \
-    glibc-static \
-    libstdc++-static \
-    libX11-devel \
-    make \
-    mesa-libGL-devel \
-    ncurses-devel \
-    neofetch \
-    nload \
-    openssl \
-    patch \
-    zlib-devel \
-    ncurses-devel.i686 \
-    readline-devel.i686 \
-    zlib-devel.i686 \
-    libX11-devel.i686 \
-    mesa-libGL-devel.i686 \
-    glibc-devel.i686 \
-    libstdc++.i686 \
-    libXrandr.i686 \
-    zip \
-    perl-Digest-SHA \
-    python2 \
-    wget \
-    lzop \
-    openssl-devel \
-    java-1.8.0-openjdk-devel \
-    ImageMagick \
-    ncurses-compat-libs \
-    schedtool \
-    lzip \
+    android-tools autoconf213 bison bzip2 ccache curl flex gawk \
+    gcc-c++ git glibc-devel glibc-static libstdc++-static libX11-devel \
+    make mesa-libGL-devel ncurses-devel neofetch nload openssl patch \
+    zlib-devel ncurses-devel.i686 readline-devel.i686 zlib-devel.i686 \
+    libX11-devel.i686 mesa-libGL-devel.i686 glibc-devel.i686 libstdc++.i686 \
+    libXrandr.i686 zip perl-Digest-SHA python2 wget lzop openssl-devel \
+    java-1.8.0-openjdk-devel ImageMagick ncurses-compat-libs lzip \
     vboot-utils
 sudo dnf autoremove -y -qq
 echo -e "\nDone."
