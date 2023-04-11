bd1dc795b0436cbd202b61b5081e1e5b726a2b08
Karan Parashar <karan@pixelos.net>
2023-04-12 01:14:28 +0530

    ubuntu-setup: Cleanup and update
    Change-Id: Ic3da0f7fbb24caf70b6d6d70b0ebbb18031f1dde

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Script to set up an Ubuntu 20.04+ server
+# Script to set up an Ubuntu 22.04+ server
 # (with minimum 16GB RAM, 8 threads CPU) for android ROM compiling
 #
 # Sudo access is mandatory to run this script
@@ -11,10 +11,6 @@
 # Usage:
 #	./ubuntu-setup.sh
 #
-
-# Go to home dir
-orig_dir=$(pwd)
-cd $HOME
 
 echo -e "Installing and updating APT packages...\n"
 sudo apt update -qq
@@ -49,13 +45,6 @@ unzip -qq platform-tools-latest-linux.zip
 rm platform-tools-latest-linux.zip
 echo -e "Done."
 
-echo -e "\nInstalling Google Drive CLI..."
-wget -q https://raw.githubusercontent.com/usmanmughalji/gdriveupload/master/gdrive
-chmod a+x gdrive
-sudo install gdrive /usr/local/bin/gdrive
-rm gdrive
-echo -e "Done."
-
 echo -e "\nInstalling apktool and JADX..."
 mkdir -p bin
 wget -q https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.6.0.jar -O bin/apktool.jar
@@ -67,117 +56,13 @@ rm jadx-1.4.4.zip
 echo 'export PATH="$HOME/jadx/bin:$PATH"' >> .bashrc
 echo -e "Done."
 
-echo -e "\nSetting up shell environment..."
-if [[ $SHELL = *zsh* ]]; then
-sh_rc=".zshrc"
-else
-sh_rc=".bashrc"
-fi
-
-cat <<'EOF' >> $sh_rc
-# Upload a file to transfer.sh
-transfer() { if [ $# -eq 0 ]; then echo -e "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"; return 1; fi
-tmpfile=$( mktemp -t transferXXX ); if tty -s; then basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g'); curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile; else curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile ; fi; cat $tmpfile; rm -f $tmpfile; echo; }
-
+cat <<'EOF' >> .bashrc
 # Super-fast repo sync
 repofastsync() { time schedtool -B -e ionice -n 0 `which repo` sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --retry-fetches=5 -j$(nproc --all) "$@"; }
 
 # List lib dependencies of any lib/bin
 list_blob_deps() { readelf -d $1 | grep "\(NEEDED\)" | sed -r "s/.*\[(.*)\]/\1/"; }
 export TZ='Asia/Kolkata'
-export USE_CCACHE=1
-export CCACHE_EXEC=/usr/bin/ccache
-
-alias gup="gdrive upload --share"
-
-function msg() {
-  echo -e "\e[1;32m$1\e[0m"
-}
-
-function helptree() {
-  if [[ -z $1 && -z $2 ]]; then
-    msg "Usage: helptree <tag> <add/pull>"
-    return
-  fi
-  kernel_version="$( cat Makefile | grep VERSION | head -n 1 | sed "s|.*=||1" | sed "s| ||g" )"
-  kernel_patchlevel="$( cat Makefile | grep PATCHLEVEL | head -n 1 | sed "s|.*=||1" | sed "s| ||g" )"
-  version=$kernel_version.$kernel_patchlevel
-  if [[ $version != "4.14" && $version != "5.4" ]]; then
-    msg "Kernel $version not supported! Only msm-5.4 and msm-4.14 are supported as of now."
-    return
-  fi
-  if [[ -z $3 ]]; then
-    spec=all
-  else
-    spec=$3
-  fi
-  if [[ $2 = "add" ]]; then
-    tree_status="Adding"
-    commit_status="Import from"
-  else
-    tree_status="Updating"
-    commit_status="Merge"
-    if [[ $spec = "all" ]]; then
-      msg "Merging kernel as of $1.."
-      git fetch https://source.codeaurora.org/quic/la/kernel/msm-$version $1
-      git merge FETCH_HEAD -m "Merge tag '$1' of msm-$version"
-    fi
-  fi
-  if [[ $spec = "wifi" || $spec = "all" ]]; then
-    for i in qcacld-3.0 qca-wifi-host-cmn fw-api; do
-      msg "$tree_status $i subtree as of $1..."
-      git subtree $2 -P drivers/staging/$i -m "$i: $commit_status tag '$1'" \
-        https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/$i $1
-    done
-  fi
-  if [[ $spec = "techpack" || $spec = "all" ]]; then
-    msg "$tree_status audio-kernel subtree as of $1..."
-    git subtree $2 -P techpack/audio -m "techpack: audio: $commit_status tag '$1'" \
-      https://source.codeaurora.org/quic/la/platform/vendor/opensource/audio-kernel $1
-    if [[ $version = "5.4" ]]; then
-      msg "$tree_status camera-kernel subtree as of $1..."
-      git subtree $2 -P techpack/camera -m "techpack: camera: $commit_status tag '$1'" \
-        https://source.codeaurora.org/quic/la/platform/vendor/opensource/camera-kernel $1
-      msg "$tree_status dataipa subtree as of $1..."
-      git subtree $2 -P techpack/dataipa -m "techpack: dataipa: $commit_status tag '$1'" \
-        https://source.codeaurora.org/quic/la/platform/vendor/opensource/dataipa $1
-      msg "$tree_status datarmnet subtree as of $1..."
-      git subtree $2 -P techpack/datarmnet -m "techpack: datarmnet: $commit_status tag '$1'" \
-        https://source.codeaurora.org/quic/la/platform/vendor/qcom/opensource/datarmnet $1
-      msg "$tree_status datarmnet-ext subtree as of $1..."
-      git subtree $2 -P techpack/datarmnet-ext -m "techpack: datarmnet-ext: $commit_status tag '$1'" \
-        https://source.codeaurora.org/quic/la/platform/vendor/qcom/opensource/datarmnet-ext $1
-      msg "$tree_status display-drivers subtree as of $1..."
-      git subtree $2 -P techpack/display -m "techpack: display: $commit_status tag '$1'" \
-        https://source.codeaurora.org/quic/la/platform/vendor/opensource/display-drivers $1
-      msg "$tree_status video-driver subtree as of $1..."
-      git subtree $2 -P techpack/video -m "techpack: video: $commit_status tag '$1'" \
-        https://source.codeaurora.org/quic/la/platform/vendor/opensource/video-driver $1
-    elif [[ $version = "4.14" ]]; then
-      if [[ $2 = "add" ]] || [ -d "techpack/data" ]; then
-        msg "$tree_status data-kernel as of $1..."
-        git subtree $2 -P techpack/data -m "techpack: data: $commit_status tag '$1'"  \
-          https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/data-kernel $1
-      fi
-    fi
-  fi
-}
-
-function addtree() {
-  if [[ -z $1 ]]; then
-    msg "Usage: addtree <tag> [optional: spec]"
-    return
-  fi
-  helptree $1 add $2
-}
-
-function updatetree() {
-  if [[ -z $1 ]]; then
-    msg "Usage: updatetree <tag> [optional: spec]"
-    return
-  fi
-  helptree $1 pull $2
-}
 EOF
 
 # Add android sdk to path
@@ -189,8 +74,8 @@ fi
 EOF
 
 # Unlimited history file
-sed -i 's/HISTSIZE=.*/HISTSIZE=-1/g' $sh_rc
-sed -i 's/HISTFILESIZE=.*/HISTFILESIZE=-1/g' $sh_rc
+sed -i 's/HISTSIZE=.*/HISTSIZE=-1/g' .bashrc
+sed -i 's/HISTFILESIZE=.*/HISTFILESIZE=-1/g' .bashrc
 
 echo -e "Done."
 
@@ -200,15 +85,11 @@ echo "set-option -g history-limit 6000" >> .tmux.conf
 # Increase maximum ccache size
 ccache -M 100G
 
-###
-### IMPORTANT !!! REPLACE WITH YOUR PERSONAL DETAILS IF NECESSARY
-###
 # Configure git
 echo -e "\nSetting up Git..."
 
-git config --global user.email "whyredfire@gmail.com"
+git config --global user.email "karan@pixelos.net"
 git config --global user.name "Karan Parashar"
-git config --global review.gerrit.pixelexperience.org.username "whyredfire"
 
 git config --global alias.cp 'cherry-pick'
 git config --global alias.c 'commit'
@@ -223,7 +104,4 @@ echo "Done."
 
 # Done!
 echo -e "\nALL DONE. Now sync sauces & start baking!"
-echo -e "Please relogin or run \`source ~/$sh_rc && source ~/.profile\` for environment changes to take effect."
-
-# Go back to original dir
-cd "$orig_dir"
+echo -e "Please relogin or run \`source ~/.bashrc && source ~/.profile\` for environment changes to take effect."
