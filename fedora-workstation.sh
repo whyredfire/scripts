51edb6ab4d4bbf7af43784dde075d7e8bb0fb182
Karan Parashar <karan@pixelos.net>
2023-04-24 01:01:27 +0530

    fedora-workstataion: [F38] Enable flatpak remote
     Fedora 38 pre-loads Flatpak by default.
    
    Change-Id: Ie56d88f76cb33dc81641be6f887de572bab53510

--- fedora-workstation.sh
+++ fedora-workstation.sh
@@ -14,8 +14,8 @@ sudo dnf install -y \
   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-# Add flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# Enable flatpak
+flatpak remote-modify --enable flathub
 
 # Install packages
 echo -e "Installing and updating dnf packages ...\n"
