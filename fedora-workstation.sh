31129001aa764c6290da25b24843821353972dbe
Karan Parashar <karan@pixelos.net>
2023-05-12 17:48:55 +0530

    fedora-workstation: Drop docker for now
    Change-Id: I766713a0bbbefce303df7d5ab675e2c3eb548b73

--- fedora-workstation.sh
+++ fedora-workstation.sh
@@ -39,23 +39,6 @@ flatpak install flathub -y com.anydesk.Anydesk \
                            org.videolan.VLC \
                            us.zoom.Zoom
 
-# docker
-sudo dnf remove -y docker \
-                   docker-client \
-                   docker-client-latest \
-                   docker-common \
-                   docker-latest \
-                   docker-latest-logrotate \
-                   docker-logrotate \
-                   docker-selinux \
-                   docker-engine-selinux \
-                   docker-engine
-
-sudo dnf -y install dnf-plugins-core
-sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo systemctl start docker
-
 # Provide host terminal to vscode flatpak
 cat > "$HOME/.config/Code/User/settings.json" <<EOF
 {
