1355d04741e9e37a24bd7eee5fada382ff41810f
Karan Parashar <karan@pixelos.net>
2023-06-03 01:57:23 +0530

    fedora-workstation: Restructure and categorize
    Change-Id: I55e371e3d8471255d3321761d6422b7303ad2dae

--- fedora-workstation.sh
+++ fedora-workstation.sh
@@ -6,95 +6,74 @@
 #        ./fedora-workstation.sh
 #
 
-cd $HOME
-
 # Enable RPM Fusion
-echo -e "Enabling RPM Fusion\n"
 sudo dnf install -y \
   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-# Enable flatpak
-flatpak remote-modify --enable flathub
-
-# Remove shipped libreoffice
-sudo dnf group remove libreoffice -y
-sudo dnf remove libreoffice-core -y
-
-# Install packages
-echo -e "Installing and updating dnf packages ...\n"
-sudo dnf update -y
-sudo dnf install -y android-tools \
-                    htop \
-                    java-latest-openjdk-devel.x86_64 \
-                    java-latest-openjdk.x86_64 \
-                    neofetch \
-                    neovim \
-                    nload \
-                    pavucontrol \
-                    ripgrep \
-                    tldr \
-                    tmate
-
-flatpak install flathub -y com.anydesk.Anydesk \
-			   com.discordapp.Discord \
-			   com.github.IsmaelMartinez.teams_for_linux \
-			   com.microsoft.EdgeDev \
-			   com.visualstudio.code \
-			   io.github.mimbrero.WhatsAppDesktop \
-			   org.libreoffice.LibreOffice \
-			   org.telegram.desktop \
-			   org.videolan.VLC \
-			   us.zoom.Zoom
-
-# Provide host terminal to vscode flatpak
-mkdir -p $HOME/.config/Code/User && touch $HOME/.config/Code/User/settings.json
-cat > "$HOME/.config/Code/User/settings.json" <<EOF
-{
-    "terminal.integrated.defaultProfile.linux": "bash",
-    "terminal.integrated.profiles.linux": {
-        "bash": {
-          "path": "/usr/bin/flatpak-spawn",
-          "args": ["--host", "--env=TERM=xterm-256color", "bash"]
-        }
-    }
-}
-EOF
-
-# battop
-echo -e "\nInstalling battop..."
-wget https://github.com/svartalf/rust-battop/releases/download/v0.2.4/battop-v0.2.4-x86_64-unknown-linux-gnu -O battop
-sudo mv battop /usr/bin/
-sudo chmod +x /usr/bin/battop
-
 # Multimedia plugins
-echo -e "\nInstalling multimedia plugins..."
 sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
 sudo dnf install -y lame\* --exclude=lame-devel
 sudo dnf group upgrade -y --with-optional Multimedia
 
-# Platform tools
-echo -e "\nInstalling Android SDK platform tools..."
-wget -q https://dl.google.com/android/repository/platform-tools-latest-linux.zip
-unzip -qq platform-tools-latest-linux.zip
-rm platform-tools-latest-linux.zip
+# Enable flatpak
+flatpak remote-modify --enable flathub
 
-echo -e "\nSetting up android udev rules..."
-git clone https://github.com/M0Rf30/android-udev-rules.git
-cd android-udev-rules
-sudo cp -v 51-android.rules /etc/udev/rules.d/51-android.rules
-sudo chmod a+r /etc/udev/rules.d/51-android.rules
-sudo cp android-udev.conf /usr/lib/sysusers.d/
-sudo systemd-sysusers
-sudo gpasswd -a $(whoami) adbusers
-sudo udevadm control --reload-rules
-sudo systemctl restart systemd-udevd.service
-adb kill-server
-rm -rf android-udev-rules
+# Install packages
+sudo dnf update -y
+sudo dnf install -y \
+  android-tools \
+  dnf-plugins-core \
+  htop \
+  java-latest-openjdk-devel.x86_64 \
+  java-latest-openjdk.x86_64 \
+  neofetch \
+  nload \
+  pavucontrol \
+  tldr \
+  tmate
+
+flatpak install flathub -y \
+  com.anydesk.Anydesk \
+  com.discordapp.Discord \
+  com.github.IsmaelMartinez.teams_for_linux \
+  com.microsoft.EdgeDev \
+  io.github.mimbrero.WhatsAppDesktop \
+  org.telegram.desktop \
+  org.videolan.VLC \
+  us.zoom.Zoom
+
+# battop
+wget https://github.com/svartalf/rust-battop/releases/download/v0.2.4/battop-v0.2.4-x86_64-unknown-linux-gnu -O battop
+sudo mv battop /usr/bin/
+sudo chmod +x /usr/bin/battop
+
+# docker
+sudo dnf remove -y \
+  docker \
+  docker-client \
+  docker-client-latest \
+  docker-common \
+  docker-latest \
+  docker-latest-logrotate \
+  docker-logrotate \
+  docker-selinux \
+  docker-engine-selinux \
+  docker-engine
+
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+
+sudo dnf install -y \
+  docker-ce \
+  docker-ce-cli \
+  containerd.io \
+  docker-buildx-plugin \
+  docker-compose-plugin
+
+sudo usermod -aG docker $USER
+sudo systemctl start docker
 
 # git
-echo -e "\nSetting up Git..."
-
 sudo dnf install 'dnf-command(config-manager)'
 sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
 sudo dnf install -y gh
@@ -114,47 +93,11 @@ git config --global alias.ck 'checkout'
 git config --global credential.helper 'cache --timeout=99999999'
 git config --global core.editor "nvim"
 
-# Setup nvchad for neovim
-rm -rf ~/.config/nvim
-rm -rf ~/.local/share/nvim
-
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.1/JetBrainsMono.zip
-unzip JetBrainsMono.zip -d ~/.fonts
-fc-cache -f -v
-
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
-
-# docker
-sudo dnf remove -y docker \
-                   docker-client \
-                   docker-client-latest \
-                   docker-common \
-                   docker-latest \
-                   docker-latest-logrotate \
-                   docker-logrotate \
-                   docker-selinux \
-                   docker-engine-selinux \
-                   docker-engine
-
-sudo dnf -y install dnf-plugins-core
-sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-
-sudo dnf install -y docker-ce \
-                    docker-ce-cli \
-                    containerd.io \ 
-                    docker-buildx-plugin \
-                    docker-compose-plugin
-
-sudo usermod -aG docker $USER
-sudo systemctl start docker
-
 # gnome shell
-echo -e "\nSetting gnome shell ..."
-
 sudo dnf install -y gnome-tweaks
-flatpak install flathub com.mattjakeman.ExtensionManager -y
+flatpak install -y flathub com.mattjakeman.ExtensionManager
 
-# To list all GNOME desktop interface settings - gsettings list-recursively org.gnome.desktop.interface
+# gsettings list-recursively org.gnome.desktop.interface
 gsettings set org.gnome.shell disable-user-extensions false
 gsettings set org.gnome.desktop.interface show-battery-percentage true
 gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
@@ -164,3 +107,53 @@ gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
 gsettings set org.gnome.desktop.interface clock-show-weekday true
 gsettings set org.gnome.desktop.interface clock-format '12h'
+
+# libreoffice
+sudo dnf group remove -y libreoffice
+sudo dnf remove -y libreoffice-core
+
+flatpak install flathub -y org.libreoffice.LibreOffice
+
+# platform tools
+wget -q https://dl.google.com/android/repository/platform-tools-latest-linux.zip
+unzip -qq platform-tools-latest-linux.zip
+rm platform-tools-latest-linux.zip
+
+git clone https://github.com/M0Rf30/android-udev-rules.git
+cd android-udev-rules
+sudo cp -v 51-android.rules /etc/udev/rules.d/51-android.rules
+sudo chmod a+r /etc/udev/rules.d/51-android.rules
+sudo cp android-udev.conf /usr/lib/sysusers.d/
+sudo systemd-sysusers
+sudo gpasswd -a $(whoami) adbusers
+sudo udevadm control --reload-rules
+sudo systemctl restart systemd-udevd.service
+adb kill-server
+rm -rf android-udev-rules
+
+# neovim
+sudo dnf install -y \
+  neovim \
+  ripgrep
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.1/JetBrainsMono.zip
+unzip JetBrainsMono.zip -d ~/.fonts
+
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+
+# vscode
+flatpak install flathub -y com.visualstudio.code
+
+mkdir -p $HOME/.config/Code/User
+touch $HOME/.config/Code/User/settings.json
+cat > "$HOME/.config/Code/User/settings.json" <<EOF
+{
+    "terminal.integrated.defaultProfile.linux": "bash",
+    "terminal.integrated.profiles.linux": {
+        "bash": {
+          "path": "/usr/bin/flatpak-spawn",
+          "args": ["--host", "--env=TERM=xterm-256color", "bash"]
+        }
+    }
+}
+EOF
