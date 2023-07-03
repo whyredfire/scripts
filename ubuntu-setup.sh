e09268c69cadd6e9f2db0e039434e570277bf46f
Karan Parashar <karan@pixelos.net>
2023-07-03 20:53:07 +0530

    ubuntu-setup: Install more packages
    Change-Id: I2ae67a25fc64871682e18d2d164a01ebe104bea3

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -20,7 +20,7 @@ sudo apt install -y -qq git-core gnupg flex bc bison build-essential zip curl zl
                         lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig imagemagick \
                         python2 python3 python3-pip python3-dev python-is-python3 schedtool ccache libtinfo5 \
                         libncurses5 lzop tmux libssl-dev neofetch patchelf apktool dos2unix git-lfs default-jdk \
-                        libxml-simple-perl rsync nano ripgrep nload
+                        libxml-simple-perl rsync nano ripgrep nload xmlstarlet binwalk
 sudo apt autoremove -y -qq
 sudo apt purge snapd -y -qq
 echo -e "\nDone."
