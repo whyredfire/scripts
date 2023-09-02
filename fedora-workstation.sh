f9f833aec98fab6405611dd2f72b170d3e02150b
Karan Parashar <karan@pixelos.net>
2023-09-03 01:36:31 +0530

    fedora-workstation: Install asusctl by default
    Change-Id: If5ef29ebfc1a29241f0d0c613bed9a5823e1686e

--- fedora-workstation.sh
+++ fedora-workstation.sh
@@ -24,6 +24,20 @@ sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-o
 sudo dnf install -y lame\* --exclude=lame-devel
 sudo dnf group upgrade -y --with-optional Multimedia
 
+# Asusctl (asus-linux.org)
+sudo dnf copr enable lukenukem/asus-linux
+
+sudo dnf update -y
+sudo dnf install kernel-devel
+sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda
+
+sudo systemctl enable nvidia-hibernate.service nvidia-suspend.service nvidia-resume.service --now
+# TODO: Update system boot configuration
+
+sudo dnf install asusctl supergfxctl
+sudo dnf update --refresh
+sudo systemctl enable supergfxd.service --now
+
 # Install packages
 sudo dnf update -y
 sudo dnf install -y \
