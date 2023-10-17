188d696b59b30ce216da4de9cdbc8bc58d8caad8
Karan Parashar <karan@pixelos.net>
2023-10-18 02:15:47 +0530

    ubuntu-setup: Install docker
    Change-Id: I8b38c325338ceb31edebb30d83642a2c0755bbb5

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -24,6 +24,25 @@ sudo apt autoremove -y -qq
 sudo apt purge snapd -y -qq
 echo -e "\nDone."
 
+echo -e "Installing and enabling docker... \n"
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+echo -e "Done."
+
 echo -e "\nInstalling git-repo..."
 wget -q https://storage.googleapis.com/git-repo-downloads/repo
 chmod a+x repo
