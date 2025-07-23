70f8453bf4f6beeea23c1e0e5e7e8bbf6d96a8c6
Karan Parashar <karan.parashar@samey.ai>
2025-07-23 12:00:50 +0530

    debloat-realmeui: Update for RealmeUI 6 (RMX3312)

--- debloat-realmeui.sh
+++ debloat-realmeui.sh
@@ -2,54 +2,39 @@
 
 # List of package names to uninstall
 packagesToUninstall=(
-    android.autoinstalls.config.oppo
-    com.coloros.activation
-    com.coloros.assistantscreen
-    com.coloros.healthcheck
+    com.android.email.partnerprovider
+    com.android.traceur
     com.coloros.securepay
-    com.debug.loggerui
     com.facebook.appmanager
     com.facebook.services
     com.facebook.system
-    com.finshell.fin
     com.glance.internet
+    com.google.android.apps.nbu.files
     com.google.android.feedback
     com.google.android.onetimeinitializer
-    com.google.android.setupwizard
+    com.google.android.partnersetup
+    com.google.android.printservice.recommendation
+    com.heytap.browser
     com.heytap.cloud
-    com.heytap.music
+    com.heytap.market
+    com.heytap.mcs
     com.heytap.pictorial
-    com.heytap.usercenter
-    com.oplus.account
+    com.microsoft.appmanager
+    com.oplus.crashbox
+    com.oplus.lfeh
     com.oplus.logkit
-    com.oplus.ocloud
-    com.oplus.omoji
-    com.oplus.onet
-    com.oplus.pay
     com.oplus.statistics.rom
-    com.oplus.synergy
-    com.oplus.themestore
+    com.oplus.wifibackuprestore
     com.oppo.quicksearchbox
     com.redteamobile.roaming
     com.tencent.soter.soterserver
-    com.android.email.partnerprovider
-)
-
-packagesToUninstallGlobally=(
-    com.finshell.fin
-    com.heytap.browser
-    com.heytap.pictorial
+    com.wapi.wapicertmanage
 )
 
 # Loop through the list and uninstall each package
 for package in "${packagesToUninstall[@]}"; do
     echo "Uninstalling package: $package"
     adb shell pm uninstall -k --user 0 "$package"
-done
-
-for package in "${packagesToUninstallGlobally[@]}"; do
-    echo "Uninstalling package: $package"
-    adb shell pm uninstall -k "$package"
 done
 
 echo "Uninstallation complete"
