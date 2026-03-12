a0c882c9d819b8b8b6b5effd550dc922dcc13725
Karan Parashar <whyredfire@gmail.com>
2026-03-13 00:21:09 +0530

    arch-setup: Install tmux and oh-my-zsh

--- arch-setup.sh
+++ arch-setup.sh
@@ -4,9 +4,9 @@
 sudo pacman -S --noconfirm \
     android-tools \
     bat htop fastfetch wget \
-    neovim \
+    neovim tmux \
     flatpak \
-    gcc \
+    gcc npm \
     noto-fonts-cjk noto-fonts-extra \
     telegram-desktop discord \
     celluloid
@@ -68,3 +68,20 @@ gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize
 gsettings set org.gnome.desktop.interface clock-show-weekday true
 gsettings set org.gnome.desktop.interface clock-format '12h'
 gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrainsMono Nerd Font Medium 10'
+
+# tmux
+git clone https://github.com/tmux-plugins/tpm --depth=1 ~/.tmux/plugins/tpm
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# dotfiles
+git clone https://github.com/whyredfire/dotfiles dotfiles
+cp -r dotfiles/.config/* ~/.config/
+rm -rf dotfiles
+
+ZSH_PATH=~/.oh-my-zsh/custom/plugins
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_PATH/zsh-autosuggestions --depth=1
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_PATH/zsh-syntax-highlighting --depth=1
+git clone https://github.com/zsh-users/zsh-completions.git $ZSH_PATH/zsh-completions --depth=1
